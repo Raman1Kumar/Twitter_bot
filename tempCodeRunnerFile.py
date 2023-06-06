bac311311deaee7394e83eb9354ac4fdf343ae4e
Raman1Kumar <kumraman555@gmail.com>
2023-06-06 17:01:40 +0530

    removed tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-sys.path.insert(2,'F:\\coding project\\insta_bot(twitter)\\upload_image\\make_poster_save_upload.py')
-# sys.path.insert(3,'F:\\coding project\\insta_bot(twitter)\\utils\\killtest.py')
--- upload_image/tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-
-import sys
-sys.path.insert(1,'F:\\coding project\\insta_bot(twitter)\\utils\\transfer_photo.py')
